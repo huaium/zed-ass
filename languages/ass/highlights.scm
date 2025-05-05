881c61170f5ca063fd3ccde5d91dddfbdd649847
huaium <huaium233@gmail.com>
2025-05-05 09:33:44 +0800

    First commit

---
+++ languages/ass/highlights.scm
@@ -0,0 +1,10 @@
+(section_name) @type ; consistency with toml
+(comment) @comment
+(key) @property
+(value) @string
+[
+  "["
+  "]"
+] @punctuation.bracket
+"," @punctuation.delimiter
+":" @operator
